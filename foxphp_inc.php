caf5ad2696e5f149f89aab73c6aa2a3bb448b00b
DimitriTheFox <Dutch4Minecrafters@gmail.com>
2017-10-05 09:32:02 +0200

    Add files via upload

---
+++ foxphp_inc.php
@@ -0,0 +1,41 @@
+<?php
+/* Stuff you may touch */
+
+//MySQL database login
+$foxdb_hostname = 'localhost';
+$foxdb_username = '';
+$foxdb_password = '';
+$foxdb_database = '';
+
+
+/* Never touch this! */
+
+//Used for version checks;
+$foxver = "0.0.1";
+
+//Used for database connections
+$foxsqli = mysqli_connect($foxdb_hostname, $foxdb_username, $foxdb_password, $foxdb_database);
+
+//Loading all functions (hardcoded for version compaxctibility)
+include('foxphp_functions/usersystem.php');
+
+
+//Loading all plugins (softcoded to make it easy)
+foreach (glob("foxphp_plugins/*.php") as $foxfilename)
+{
+    include $foxfilename;
+}
+
+//Blocked this page from being loaded in the browser
+if(strpos($_SERVER['PHP_SELF'], 'foxphp_inc.php') !== false)
+{
+	echo "<body oncontextmenu='return false;'>";
+	echo "<center>";
+	echo "<h1>What are you doing here?</h1>";
+	echo "<img src='logo.png' height='100vh' draggable='false'><br>";
+	echo "<a href='index'>Back to homepage</a>";
+	echo "</center>";
+	echo "</body>";
+	exit();
+}
+?>
---
+++ foxphp_setup.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>FoxPHP Setup</title>
+</head>
+
+<body>
+	<?php
+		$setup = $_POST['setup'];
+		if(isset($setup))
+		{
+			require('foxphp_inc.php');
+			
+			$query1 = "CREATE TABLE IF NOT EXISTS `foxphp_users` (
+						`UserID` int(11) NOT NULL,
+						  `username` varchar(9999) COLLATE utf8_unicode_ci NOT NULL,
+						  `password` varchar(9999) COLLATE utf8_unicode_ci NOT NULL,
+						  `level` int(1) NOT NULL DEFAULT '0'
+						) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;";
+		
+			$query2 = "ALTER TABLE `foxphp_users`
+ 						ADD PRIMARY KEY (`UserID`);";
+			
+			$query3 = "ALTER TABLE `foxphp_users`
+						MODIFY `UserID` int(11) NOT NULL AUTO_INCREMENT;";
+			
+			if(!mysqli_query($foxsqli, $query1) || !mysqli_query($foxsqli, $query2) || !mysqli_query($foxsqli, $query3))
+			{
+				echo "<h1 style='color=red'>Setup failed, did you configure a databse in foxphp_inc.php?</h1>";
+				exit();
+			}
+		}
+	?>
+	
+	<h1>FoxPHP Setup</h1>
+	<img src='logo.png' height='100vh' draggable='false'><br>
+	<p>
+		This setup is an automated process. But before you press Start Setup make sure you configured a database in foxphp_inc.php.
+	</p>
+	<form method="post">
+		<input type="submit" name="setup" value="Start Setup">
+	</form>
+</body>
+</html>
